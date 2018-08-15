6bff8d945a617418808eaaf04828103f925771ee
Roderick Menor <rodpajmen@gmail.com>
2018-08-14 21:42:08 -1000

    Pending

--- exercises.py
+++ exercises.py
@@ -8,7 +8,17 @@
 
 #1. Declare a variable named weather and assign it a string value of 'rain'. Next create a conditional statement that will check the weather and print 'Bring an umbrella' if weather equals 'rain'.
 
+weather = 'rain'
+if(weather == 'rain'):
+    print('Bring an umbrella')
+
 #2 Declare a variable named score and assign it a number value of 70. Next create a conditional statement that will check the score and print 'You pass!' if the score is 70 and above and print 'Study harder!' if the score is less than 70.
+
+score = 70
+if(score >= 70):
+    print('You pass!')
+else:
+    print('Study Harder!')
 
 #3. Declare a variable named download_speed and assign it a data value of 50. Next create a conditional statement that will check the download speed and print the following based on the condition:
 
@@ -16,15 +26,38 @@
 # <=100: 'Premium Package'
 # >100: 'Platinum Package'
 
+download_speed = 50
+if download_speed <= 50:
+    print('Basic Package')
+elif download_speed <= 100:
+    print('Premium Package')
+else: 
+    print('Platinum Package')
+
  #4 Function - check_password
  #Create a function named check_password which takes a parameter password.
 
  #The function will return true if the password passed into the function is equal to 'qwerty'. Declare a variable named password_result and print your result.
 
+def check_password(password):
+    if(password == 'qwerty'):
+        return True
+password = 'qwerty'
+password_result = (password == 'qwerty')
+print(password_result)
+
 #5 Function check_login
 #Create a function named check_login which takes a parameter login.
 
 #The function will print 'Login Success' if the login passed into the function is equal to 'DevLeague' and print 'Re-enter Login' if it doesn't.
+
+def check_login(login):
+    if(login == 'DevLeague'):
+        return print('Login Success')
+    else:
+        return print('Re-enter Login')
+check_login =('DevLeague')
+
 
 #6 Function malware_type
 #Create a function named malware_type which takes a parameter malware. 
@@ -33,6 +66,9 @@
 #if malware is adware: 'Low Threat'
 #if malware is virus: 'Do not share files'
 #default message 'I hope you backed up your data'
+
+malware_type = 'malware'
+
 
 #7 Function encryption
 #Create a function named encryption which takes a parameter keys.
